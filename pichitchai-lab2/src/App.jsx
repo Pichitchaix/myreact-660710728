f0bda1417c5ad57f071e9c85ed5adb90afbc5494
Pichitchaix <jaemjumrat_p@su.ac.th>
2025-08-28 12:02:42 +0700

    lab2-compenents

---
+++ pichitchai-lab2/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ComponentExamples from './components/ComponentExamples';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <ComponentExamples />
+            <a href="https://react.dev">learn react</a> 
+        </div>
+    );
+}
+
+export default App;
